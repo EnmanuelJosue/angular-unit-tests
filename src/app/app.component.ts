d799d25bf4f743bac38f6a6b1e3b5fa3bf330fa1
Enmanuel <enmanueljosue2612@hotmail.com>
2022-06-03 18:03:57 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { Calculator } from './calculator';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(
+  ) {
+
+  }
   title = 'ng-testing-services';
+  ngOnInit() {
+    const calculator = new Calculator();
+    const rta = calculator.multiply(1,4)
+    const rta2 = calculator.divide(3,0)
+
+  }
 }
